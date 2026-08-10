67cf6821dd5a66aae94b5e6b60b9cca03bee72c7
Lectio-Divina <octave.resolve.0g@icloud.com>
2026-08-10 13:59:16 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -351,7 +351,7 @@ const toggleTimer = () => {
   </div>
 
   <div className="copyright-notice">
-    © 2026 Lectio Divina · CC BY-NC
+    © 2026 Lectio Divina · CC BY-NC Support: octave.resolve.0g@icloud.com
   </div>
 </footer>
 
